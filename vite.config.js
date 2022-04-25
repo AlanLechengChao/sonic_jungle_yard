b1f619d843dfe119ba54bc86fc9f377277213920
ALanLechengChao <lechengchao@163.com>
2022-04-25 12:21:45 -0400

    asdf

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sonic_jungle_yard/"
+  base: "/sonic_jungle_yard/",
   plugins: [vue()],
   resolve: {
     alias: {
